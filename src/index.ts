76e9d59c4ed60268abedce91c1c8b5513070d73c
Justin Ang <jrang@sfu.ca>
2023-04-01 19:44:02 -0700

    removed annoying eslint extension

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
-import dotenv from 'dotenv';
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
-// eslint-disable-next-line import/extensions
-import isValidUrl from './utils';
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import isValidUrl from "./utils";
 
 dotenv.config();
 const app = express();
@@ -16,21 +15,21 @@ const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
 // Your first API endpoint
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ greeting: "hello API" });
 });
 
-app.post('/api/shorturl', async (req: Request, res: Response) => {
+app.post("/api/shorturl", async (req: Request, res: Response) => {
   const originalURL = req.body.url;
   if (!isValidUrl(originalURL)) {
-    return res.json({ error: 'invalid url' });
+    return res.json({ error: "invalid url" });
   }
 
   const existingUrl = await prisma.url.findUnique({
@@ -50,7 +49,7 @@ app.post('/api/shorturl', async (req: Request, res: Response) => {
   return res.json({ original_url: originalURL, short_url: existingUrl.id });
 });
 
-app.get('/api/shorturl/:id', async (req: Request, res: Response) => {
+app.get("/api/shorturl/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const url = await prisma.url.findUnique({
     where: { id: parseInt(id, 10) },
@@ -58,7 +57,7 @@ app.get('/api/shorturl/:id', async (req: Request, res: Response) => {
 
   if (!url) {
     // If the URL does not exist, return an error response
-    return res.status(404).json({ error: 'URL not found' });
+    return res.status(404).json({ error: "URL not found" });
   }
 
   return res.redirect(url.originalURL);
